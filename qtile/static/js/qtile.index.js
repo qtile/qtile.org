a6b9d4b6b4fa2d083e1a9e5be242d1fb68cff712
dmpayton <derek.payton@gmail.com>
2014-07-14 22:27:11 -0700

    Add a title attr to the example config toggle.

--- qtile/static/js/qtile.index.js
+++ qtile/static/js/qtile.index.js
@@ -8,6 +8,7 @@ var init_demo = function(){
 
             $config.find('.btn-toggle')
                 .data('expanded', false)
+                .attr('title', 'Expand')
                 .on('click', function(){
                     var $this = $(this),
                         $icon = $this.find('span.fa'),
@@ -17,6 +18,7 @@ var init_demo = function(){
                     $this.data('expanded', !$this.data('expanded'));
                     var newHeight = $this.data('expanded') ? scrollHeight : 340;
 
+                    $this.attr('title', $this.data('expanded') ? 'Collapse' : 'Expand');
                     $icon.toggleClass('fa-expand').toggleClass('fa-compress');
                     $pre.animate({height: newHeight + 'px'});
                 });

3964ec8b8cbb51c98ac2d309b9a1928cfd39d608
dmpayton <derek.payton@gmail.com>
2014-07-22 01:43:26 -0700

    Update demo config.

--- qtile/includes/config.py
+++ qtile/includes/config.py
@@ -1,40 +1,116 @@
-from libqtile.config import Key, Screen, Group, Drag, Click
+from libqtile import bar, hook, layout, widget
 from libqtile.command import lazy
-from libqtile import layout, bar, widget, hook
+from libqtile.config import Click, Drag, Group, Key, Screen
 
-MOD = 'mod4'
+wmname = 'qtile'
+mod = 'mod4'
 
-screens = [Screen(
-    top=bar.Bar([
-        widget.GroupBox(),
-        widget.WindowName(),
-        widget.Systray(),
-        widget.Clock('%a %d %b %I:%M %p'),
-    ], size=30)
-)]
-
+# Key bindings
 keys = [
-    Key([MOD, 'control'], 'r', lazy.restart()),
-    Key([MOD, 'control'], 'q', lazy.shutdown()),
+    # Window manager controls
+    Key([mod, 'control'], 'r', lazy.restart()),
+    Key([mod, 'control'], 'q', lazy.shutdown()),
+    Key([mod], 'r', lazy.spawncmd()),
+    Key([mod], 'Return', lazy.spawn('xterm')),
+    Key([mod], 'w',      lazy.window.kill()),
 
-    Key([MOD], 'w', lazy.window.kill()),
-    Key([MOD], 'f', lazy.window.toggle_floating()),
+    Key([mod], 'Tab', lazy.layout.next()),
+    Key([mod], 'Left', lazy.screen.prevgroup()),
+    Key([mod], 'Right', lazy.screen.nextgroup()),
 
-    Key([MOD], 'Left', lazy.screen.prevgroup()),
-    Key([MOD], 'Right', lazy.screen.nextgroup()),
+    # Layout modification
+    Key([mod, 'control'], 'space', lazy.window.toggle_floating()),
 
-    Key([MOD], 'Up', lazy.layout.next()),
-    Key([MOD], 'Down', lazy.layout.previous()),
-    Key([MOD], 'Tab', lazy.nextlayout()),
+    # Switch between windows in current stack pane
+    Key([mod], 'k', lazy.layout.down()),
+    Key([mod], 'j', lazy.layout.up()),
+
+    # Move windows up or down in current stack
+    Key([mod, 'control'], 'k', lazy.layout.shuffle_down()),
+    Key([mod, 'control'], 'j', lazy.layout.shuffle_up()),
+
+    # Switch window focus to other pane(s) of stack
+    Key([mod], 'space', lazy.layout.next()),
+
+    # Toggle between different layouts as defined below
+    Key([mod], 'Tab',    lazy.nextlayout()),
 ]
 
-groups = []
-for i in range(1, 10):
-    groups.append(Group(i))
-    keys.append(Key([MOD], i, lazy.group[i].toscreen()))
-    keys.append(Key([MOD, 'MOD1'], i, lazy.window.togroup(i)))
+# Mouse bindings and options
+mouse = (
+    Drag([mod], 'Button1', lazy.window.set_position_floating(),
+        start=lazy.window.get_position()),
+    Drag([mod], 'Button3', lazy.window.set_size_floating(),
+        start=lazy.window.get_size()),
+)
 
+bring_front_click = True
+cursor_warp = False
+follow_mouse_focus = True
+
+# Groups
+groups = [
+    Group('a'),
+    Group('s'),
+    Group('d'),
+    Group('f'),
+    Group('u'),
+    Group('i'),
+    Group('o'),
+    Group('p'),
+]
+for i in groups:
+    # mod + letter of group = switch to group
+    keys.append(Key([mod], i.name, lazy.group[i.name].toscreen()))
+
+    # mod + shift + letter of group = switch to & move focused window to group
+    keys.append(Key([mod, 'shift'], i.name, lazy.window.togroup(i.name)))
+
+dgroups_key_binder = None
+dgroups_app_rules = []
+
+# Layouts
 layouts = [
-    layout.Stack(stacks=2, border_width=1),
     layout.Max(),
+    layout.Stack(num_stacks=2),
+    layout.Tile(),
+    layout.RatioTile(),
+    layout.Matrix(),
 ]
+
+floating_layout = layout.Floating()
+
+# Screens and widget options
+screens = [
+    Screen(
+        bottom=bar.Bar([
+            widget.GroupBox(),
+            widget.Prompt(),
+            widget.WindowName(),
+            widget.Systray(),
+            widget.Clock(format='%a %d %b %I:%M %p'),
+        ], size=30),
+    ),
+]
+
+widget_defaults = dict(
+    font='Arial',
+    fontsize=16,
+    padding=3,
+)
+
+auto_fullscreen = True
+
+
+def main(qtile):
+    ''' This function is called when Qtile starts. '''
+    pass
+
+
+@hook.subscribe.client_new
+def floating_dialogs(window):
+    ''' Set dialog windows to floating. '''
+    dialog = window.window.get_wm_type() == 'dialog'
+    transient = window.window.get_wm_transient_for()
+    if dialog or transient:
+        window.floating = True

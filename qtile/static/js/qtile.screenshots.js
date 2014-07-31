ba35c4f6a35c8a218035b4d9ec09ef02dd189b14
dmpayton <derek.payton@gmail.com>
2014-07-30 18:13:32 -0700

    Implement lightbox functionality on the screneshots.

--- qtile/static/js/qtile.screenshots.js
+++ qtile/static/js/qtile.screenshots.js
@@ -14,7 +14,6 @@ var init_screenshots = function(){
 
             $gallery.find('.gallery-item').each(function(){
                 var $this = $(this);
-                console.log($this);
                 if($this.hasClass('width-1')){
                     $this.addClass('col-xs-12 col-sm-4 col-md-3');
                 } else if($this.hasClass('width-2')){
@@ -29,6 +28,42 @@ var init_screenshots = function(){
 
             $gallery.imagesLoaded(function(){ init_masonry($gallery); });
             $(window).resize(function(){ init_masonry($gallery); });
+        });
+    })(jQuery);
+};
+
+var init_screenshot_lightbox = function(){
+    (function($){
+        $(document).ready(function(){
+            $('.screenshot.thumbnail a.fullsize').on('click', function(){
+                console.log('WTF IS THIS');
+                var $this = $(this),
+                    $modal = $('#lightbox');
+
+                $modal.find('.modal-title, .modal-body, .modal-footer').empty();
+
+                $modal.find('.modal-title').text($this.data('title'));
+                $modal.find('.modal-body').html(
+                    $('<a>')
+                        .attr('href', $this.attr('href'))
+                        .html(
+                            $('<img>')
+                                .attr('src', $this.attr('href'))
+                                .attr('alt', $this.data('title'))
+                                .addClass('thumbnail')
+                        )
+                );
+
+                if($this.data('config')) {
+                    $modal.find('.modal-footer').html(
+                        $('<a>')
+                            .text('View Config')
+                            .attr('href', $this.data('config'))
+                    );
+                }
+                $modal.modal({show: true});
+                return false;
+            });
         });
     })(jQuery);
 };

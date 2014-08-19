a434125aa25306e62cb015dee437c76bc1c6f9a6
dmpayton <derek.payton@gmail.com>
2014-08-18 19:25:05 -0700

    Updates to homepage.

--- qtile/settings/common.py
+++ qtile/settings/common.py
@@ -135,9 +135,6 @@ QTILE_VERSION = os.environ.get('QTILE_VERSION', '0.7.0')
 QTILE_DOWNLOAD = os.environ.get('QTILE_DOWNLOAD',
     'https://github.com/qtile/qtile/archive/v{0}.tar.gz'.format(QTILE_VERSION))
 
-QTILE_RELEASE_NOTES = os.environ.get('QTILE_RELEASE_NOTES',
-    'http://docs.qtile.org/en/latest/releases/{0}.html'.format(QTILE_VERSION))
-
 QTILE_RELEASE_DATE = datetime.datetime.strptime(
     os.environ.get('QTILE_RELEASE_DATE', '2014-03-30'), '%Y-%m-%d').date()
 

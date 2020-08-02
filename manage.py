5111ad1e19990fa2c0ca2c551c8998d0b595d99b
dmpayton <derek.payton@gmail.com>
2020-08-02 15:49:07 -0700

    Use dotenv to look for a .env file for local development.

--- manage.py
+++ manage.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python
 import os
+import pprint
 import sys
 
+import dotenv
+
 if __name__ == "__main__":
+    # Run output through the pretty printer.
+    sys.displayhook = pprint.pprint
+
+    # Load our .env
+    dotenv.load_dotenv(dotenv.find_dotenv())
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "qtile.settings.local")
 
     from django.core.management import execute_from_command_line

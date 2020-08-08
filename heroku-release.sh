0ac67d7da508cb0b9f2bb8e7a68a900163417aa5
dmpayton <derek.payton@gmail.com>
2020-08-08 16:45:34 -0700

    Move post_compile scripts to the release process.

---
+++ heroku-release.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+# The post_compile hook is run by heroku-buildpack-python
+
+echo "-----> Running release process"
+
+# Install front-end dependencies
+echo "-----> Installing front-end components"
+npm install -g bower less
+bower install --config.interactive=false
+
+# Compile static assets
+echo "-----> Collecting static files"
+python manage.py collectstatic --noinput --traceback 2>&1 | sed '/^Copying/d;/^$/d;/^ /d'
+
+echo "-----> Compressing static files"
+python manage.py compress --traceback 2>&1
+
+echo "-----> Creating screenshot thumbnails"
+python manage.py process_screenshots --traceback 2>&1
+
+echo "-----> Release process done"
